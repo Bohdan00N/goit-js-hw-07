2f6f4f45e3530957eb3f230de045b3243960e018
Bohdan <nalivkin.bog@gmail.com>
2023-01-03 16:31:47 +0200

    7.1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+(function createGalleryMarkup() {
+  const elementCreateGallery = galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    `;
+    })
+    .join("");
+  galleryEl.insertAdjacentHTML("beforeend", elementCreateGallery);
+})();
+
+new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
